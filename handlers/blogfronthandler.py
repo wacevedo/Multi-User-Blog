c817224cc0606c93328d6c267c134eb923dc6efe
Willson Acevedo <wacevedo@intellisys.com.do>
2017-04-20 10:10:03 -0400

    adding navbar to blog

--- handlers/blogfronthandler.py
+++ handlers/blogfronthandler.py
@@ -3,5 +3,8 @@ from models.post import Post
 
 class BlogFront(Handler):
     def get(self):
+      username = None
+      if self.user:
+        username = self.user.name
       posts = Post.all().order('-created') #select * from Post order by created desc limit 10
-      self.render('frontPost.html', posts = posts) #here front.html
+      self.render('frontPost.html', posts = posts, username = username) #here front.html
